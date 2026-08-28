d9dd283dc128dd1299750019d8057e812c4e68f8
HWG.Tech.Grafik <florian_moser@gmx.at>
2026-08-28 20:01:25 +0200

    Add files via upload

--- 02215062/sw.js
+++ 02215062/sw.js
@@ -6,7 +6,7 @@
    Bei neuer Version unten CACHE hochzählen – der alte Cache wird dann
    automatisch verworfen.
    ===================================================================== */
-const CACHE = "billy-ledger-02215062-2.6.1";
+const CACHE = "billy-ledger-02215062-2.6.3";
 
 // App-Gerüst + CDN-Bibliotheken (Versionen wie in der index.html!)
 const DATEIEN = [
